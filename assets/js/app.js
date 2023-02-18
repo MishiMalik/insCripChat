f18aea5cc5b46385f8536abbc3894461395e2c1d
Mehwish <mehwishgul715@gmail.com>
2023-02-18 13:43:31 +0500

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,46 @@
 
 
 $(".sign-up").click(function () {
-    $(".verification-message").css("display","block");
-    setTimeout(function() { 
-        $(".verification-message").css("display","none");
+    $(".verification-message").css("display", "block");
+    setTimeout(function () {
+        $(".verification-message").css("display", "none");
     }, 2000);
 });
+
+// ====================================video play/pause
+var vid = document.getElementById("video");
+function playVid() {
+    vid.play();
+}
+
+function pauseVid() {
+    vid.pause();
+}
+$(".play-btn-div").on("click", function () {
+    $(this).css("display", "none");
+    $(".pause-btn-div").css("display", "block");
+
+})
+$(".pause-btn-div").on("click", function () {
+    $(this).css("display", "none");
+    $(".play-btn-div").css("display", "block");
+})
+
+
+// var vid = document.getElementById("video2");
+// function playVid2() {
+//     vid.play();
+// }
+
+// function pauseVid2() {
+//     vid.pause();
+// }
+// $(".play-btn-div2").on("click", function () {
+//     $(this).css("display", "none");
+//     $(".pause-btn-div2").css("display", "block");
+
+// })
+// $(".pause-btn-div2").on("click", function () {
+//     $(this).css("display", "none");
+//     $(".play-btn-div2").css("display", "block");
+// })
